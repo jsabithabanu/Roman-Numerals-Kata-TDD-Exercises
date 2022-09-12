b619272231f958a046a7be339de0967127063fcd
jsabithabanu <jsabithabanu@gmail.com>
2022-09-12 22:01:23 +0100

    Added test cases to convert bigger Roman numerals into numbers

--- RomanNumeralsKataTDDExercise.Tests/RomanToNumberConverterTests.cs
+++ RomanNumeralsKataTDDExercise.Tests/RomanToNumberConverterTests.cs
@@ -49,5 +49,28 @@ namespace RomanNumeralsKataTDDExercise.Tests
             romanToNumberConverter.RomanToNumber("V").Should().Be(5);
         }
 
+        [Test]
+        public void Testing_To_Convert_Roman_Numeral_To_Number_Fifty_Four()
+        {
+            romanToNumberConverter.RomanToNumber("LIV").Should().Be(54);
+        }
+
+        [Test]
+        public void Testing_To_Convert_Roman_Numeral_To_Number_One_Hundred_And_Twenty_Nine()
+        {
+            romanToNumberConverter.RomanToNumber("CXXIX").Should().Be(129);
+        }
+
+        [Test]
+        public void Testing_To_Convert_Roman_Numeral_To_Number_Four_Hundred_And_Forty_Nine()
+        {
+            romanToNumberConverter.RomanToNumber("CDXLIX").Should().Be(449);
+        }
+
+        [Test]
+        public void Testing_To_Convert_Roman_Numeral_To_Number_One_Thousand_Three_Hundred_And_Sixty_Seven()
+        {
+            romanToNumberConverter.RomanToNumber("MCCCLXVII").Should().Be(1367);
+        }
     }
 }
--- RomanNumeralsKataTDDExercise/RomanToNumberConverter.cs
+++ RomanNumeralsKataTDDExercise/RomanToNumberConverter.cs
@@ -14,7 +14,12 @@ namespace RomanNumeralsKataTDDExercise
             var RomanMap = new Dictionary<string, int>
             {
                 {"I", 1},
-                {"V", 5}
+                {"V", 5},
+                {"X", 10},
+                {"L", 50},
+                {"C", 100},
+                {"D", 500},
+                {"M", 1000}
             };
 
             int number = 0;
